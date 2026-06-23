7c4966c97c66469eb0010c89a9f59692fb3312ca
juanmendestrabalho-hub <juanmendestrabalho@gmail.com>
2026-06-22 21:55:40 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,24 @@
-.
+function loginView() {
+  return `
+    <div class="container">
+      <h2>Login</h2>
+      <input id="user" placeholder="Usuário">
+      <input id="pass" type="password" placeholder="Senha">
+      <button onclick="login()">Entrar</button>
+    </div>
+  `;
+}
+
+function login() {
+  const user = document.getElementById("user").value;
+
+  if (!user) return alert("Digite usuário");
+
+  localStorage.setItem("user", user);
+  loadDashboard();
+}
+
+function logout() {
+  localStorage.removeItem("user");
+  location.reload();
+}
