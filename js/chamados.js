8743be37741ac7b5a6003a40c4c81f947e824d38
juanmendestrabalho-hub <juanmendestrabalho@gmail.com>
2026-06-22 21:56:01 -0300

    Update chamados.js

--- js/chamados.js
+++ js/chamados.js
@@ -1,1 +1,27 @@
 
+let chamados = JSON.parse(localStorage.getItem("chamados")) || [];
+
+function loadChamados() {
+  render(`
+    ${navbar()}
+    <div class="container">
+      <h1>Chamados</h1>
+
+      <input id="titulo" placeholder="Novo chamado">
+      <button onclick="addChamado()">Criar</button>
+
+      ${chamados.map(c => `
+        <div class="card">
+          ${c}
+        </div>
+      `).join("")}
+    </div>
+  `);
+}
+
+function addChamado() {
+  const titulo = document.getElementById("titulo").value;
+  chamados.push(titulo);
+  localStorage.setItem("chamados", JSON.stringify(chamados));
+  loadChamados();
+}
