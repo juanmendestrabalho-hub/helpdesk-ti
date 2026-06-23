e4e9871c8a5a34f36c29461d0757789970a92581
juanmendestrabalho-hub <juanmendestrabalho@gmail.com>
2026-06-22 21:56:40 -0300

    Update maquinas.js

--- js/maquinas.js
+++ js/maquinas.js
@@ -1,1 +1,18 @@
-.
+const maquinas = [
+  "SCC98929CP",
+  "SCC85150CP",
+  "SCC98952CP"
+];
+
+function loadMaquinas() {
+  render(`
+    ${navbar()}
+    <div class="container">
+      <h1>Máquinas</h1>
+
+      ${maquinas.map(m => `
+        <div class="card">${m}</div>
+      `).join("")}
+    </div>
+  `);
+}
